77ec028e0c0d7d05e5260756450b5e00217d98c1
Bernardo Emilio Valderrama Canto <emiliocanto13@gamil.com>
2022-10-18 23:59:00 -0300

    Version V2 de inicio

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import crearDepartamento
+from .views import crearDepartamento, Home
 
 urlpatterns = [
+    path('',Home, name = 'index'),
     path('crear_departamento/',crearDepartamento, name='crear_departamento')
 ]
